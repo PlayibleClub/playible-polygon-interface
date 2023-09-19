e688efac6df42e3832dd00d92a285fb6e451bddd
Lilith <nekopass.420loli@gmail.com>
2023-09-20 05:40:23 +0800

    updated to use transpiler

--- next.config.js
+++ next.config.js
@@ -1,23 +1,24 @@
 const withImages = require('next-images');
+const withTM = require('next-transpile-modules')(['@playibleclub/playible-interface-components']);
 
-nextConfig = {
-  images: {
-    domains: [
-      'playible-api-production.s3.ap-southeast-1.amazonaws.com',
-      'playible-game-image.s3-ap-southeast-1.amazonaws.com',
-      'playible-game-image.s3.ap-southeast-1.amazonaws.com',
-      'playible-api-dev.s3.ap-southeast-1.amazonaws.com',
-      's3-us-west-2.amazonaws.com',
-      'ipfs.filebase.io',
-    ],
-  },
-  env: {
-    NEAR_ENV: process.env.NEAR_ENV,
-    GRAPHQL_URL: process.env.GRAPHQL_URL,
-    ADMIN: process.env.ADMIN,
-    AWS_ACCESS_KEY: process.env.AWS_ACCESS_KEY,
-    AWS_SECRET_KEY: process.env.AWS_SECRET_KEY,
-  },
-};
-
-module.exports = withImages(nextConfig);
+module.exports = withImages(
+  withTM({
+    images: {
+      domains: [
+        'playible-api-production.s3.ap-southeast-1.amazonaws.com',
+        'playible-game-image.s3-ap-southeast-1.amazonaws.com',
+        'playible-game-image.s3.ap-southeast-1.amazonaws.com',
+        'playible-api-dev.s3.ap-southeast-1.amazonaws.com',
+        's3-us-west-2.amazonaws.com',
+        'ipfs.filebase.io',
+      ],
+    },
+    env: {
+      NEAR_ENV: process.env.NEAR_ENV,
+      GRAPHQL_URL: process.env.GRAPHQL_URL,
+      ADMIN: process.env.ADMIN,
+      AWS_ACCESS_KEY: process.env.AWS_ACCESS_KEY,
+      AWS_SECRET_KEY: process.env.AWS_SECRET_KEY,
+    },
+  })
+);
