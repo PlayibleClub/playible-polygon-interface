da67ac690b2da11d01b7b78fc4955dc3d71be2d4
Kendrell Derek Acibar <59480053+ciirno@users.noreply.github.com>
2023-09-22 02:04:30 +0800

    Changed wallet selector context to use metamask

--- contexts/WalletSelectorContext.tsx
+++ contexts/WalletSelectorContext.tsx
@@ -1,106 +1,50 @@
-import React, { useCallback, useContext, useEffect, useState } from 'react';
-import { map, distinctUntilChanged } from 'rxjs';
-import { setupWalletSelector } from '@near-wallet-selector/core';
-import { WalletSelector, AccountState } from '@near-wallet-selector/core';
-import { setupModal } from '@near-wallet-selector/modal-ui';
-import { WalletSelectorModal } from '@near-wallet-selector/modal-ui';
-import { setupNearWallet } from '@near-wallet-selector/near-wallet';
-import { setupNeth } from '@near-wallet-selector/neth';
-import { setupLedger } from '@near-wallet-selector/ledger';
-import { setupNightly } from '@near-wallet-selector/nightly';
-import { setupMyNearWallet } from '@near-wallet-selector/my-near-wallet';
-import { setupMeteorWallet } from '@near-wallet-selector/meteor-wallet';
-import { getConfig, getContract } from '../utils/near';
-import { MINTER_NFL } from '../data/constants/nearContracts';
+import React, { createContext, useContext, useState, useCallback } from 'react';
+import { ethers } from 'ethers';
 
 declare global {
   interface Window {
-    selector: WalletSelector;
-    modal: WalletSelectorModal;
+    ethereum: any;
   }
 }
+// Define the context
 interface WalletSelectorContextValue {
-  selector: WalletSelector;
-  modal: WalletSelectorModal;
-  accounts: Array<AccountState>;
   accountId: string | null;
+  connectWallet: () => void;
+  disconnectWallet: () => void;
 }
 
 const WalletSelectorContext = React.createContext<WalletSelectorContextValue | null>(null);
 
-// @ts-ignore:next-line
-export const WalletSelectorContextProvider: React.FC = ({ children }) => {
-  const [selector, setSelector] = useState<WalletSelector | null>(null);
-  const [modal, setModal] = useState<WalletSelectorModal | null>(null);
-  const [accounts, setAccounts] = useState<Array<AccountState>>([]);
+// Context Provider component
+export const WalletSelectorContextProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  const [accountId, setAccountId] = useState<string | null>(null);
 
-  const init = useCallback(async () => {
-    const _selector = await setupWalletSelector({
-      network: getConfig(),
-      debug: true,
-      modules: [
-        setupNearWallet(),
-        setupMyNearWallet(),
-        setupNeth(),
-        setupLedger(),
-        setupNightly(),
-        setupMeteorWallet(),
-      ],
-    });
-    const _modal = setupModal(_selector, {
-      contractId: getContract(MINTER_NFL),
-      methodNames: [...MINTER_NFL.interface.viewMethods, ...MINTER_NFL.interface.changeMethods],
-    });
-    const state = _selector.store.getState();
-
-    setAccounts(state.accounts);
-
-    window.selector = _selector;
-    window.modal = _modal;
-
-    setSelector(_selector);
-    setModal(_modal);
+  // Connect wallet function
+  const connectWallet = useCallback(async () => {
+    try {
+      // Request access to user's MetaMask accounts
+      await window.ethereum.request({ method: 'eth_requestAccounts' });
+      const accounts = await window.ethereum.request({ method: 'eth_accounts' });
+      setAccountId(accounts[0] || null);
+    } catch (error) {
+      console.error('Error connecting to MetaMask:', error);
+    }
   }, []);
 
-  useEffect(() => {
-    init().catch((err) => {
-      console.error(err);
-      alert('Failed to initialise wallet selector');
-    });
-  }, [init]);
+  // Disconnect wallet function
+  const disconnectWallet = useCallback(() => {
+    setAccountId(null);
+  }, []);
 
-  useEffect(() => {
-    if (!selector) {
-      return;
-    }
-
-    const subscription = selector.store.observable
-      .pipe(
-        map((state) => state.accounts),
-        distinctUntilChanged()
-      )
-      .subscribe((nextAccounts) => {
-        console.log('Accounts Update', nextAccounts);
-
-        setAccounts(nextAccounts);
-      });
-
-    return () => subscription.unsubscribe();
-  }, [selector]);
-
-  if (!selector || !modal) {
-    return null;
-  }
-
-  const accountId = accounts.find((account) => account.active)?.accountId || null;
-
+  // Render children with context value
   return (
     <WalletSelectorContext.Provider
       value={{
-        selector,
-        modal,
-        accounts,
         accountId,
+        connectWallet,
+        disconnectWallet,
       }}
     >
       {children}
@@ -108,8 +52,9 @@ export const WalletSelectorContextProvider: React.FC = ({ children }) => {
   );
 };
 
+// Custom hook to consume the context
 export function useWalletSelector() {
-  const context = useContext(WalletSelectorContext);
+  const context = React.useContext(WalletSelectorContext);
 
   if (!context) {
     throw new Error('useWalletSelector must be used within a WalletSelectorContextProvider');
