e911565f87c8a14f104f322caf8b0cce8644638f
Kendrell Derek Acibar <59480053+ciirno@users.noreply.github.com>
2023-09-21 18:18:32 +0800

    add polygon constant

---
+++ data/constants/polygonConstants.ts
@@ -0,0 +1,13 @@
+export const POL141USDC = {
+  title: 'USDC',
+  mainnet: '',
+  testnet: '0xbAfd91F2AB0d596f55DD74657381A9D9E9029777',
+  decimals: 1000000000000000000,
+}; //ERC-20 USDC for Polygon
+
+export const POL141USDT = {
+  title: 'USDT',
+  mainnet: '',
+  testnet: '',
+  decimals: 1000000,
+}; //placeholder
