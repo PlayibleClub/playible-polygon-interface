02331e7a63a1270549e5ed9ab760d9bb60ff84e5
Lilith <nekopass.420loli@gmail.com>
2023-09-22 02:01:36 +0800

    Update Container.tsx

--- components/containers/Container.tsx
+++ components/containers/Container.tsx
@@ -4,15 +4,12 @@ import DesktopHeaderBase from '../headers/DesktopHeaderBase';
 import Navbar from '../navbars/Navbar';
 import HeaderBase from '../headers/HeaderBase';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { useWalletSelector } from 'contexts/WalletSelectorContext';
+// import { useWalletSelector } from 'contexts/WalletSelectorContext';
 import { isAdminChecker } from 'utils/admin';
 import Head from 'next/head';
 
 const Container = (props) => {
   const { activeName, children } = props;
-  const { selector, accountId } = useWalletSelector();
-
-  const isAdmin = isAdminChecker(accountId);
 
   return (
     <div className="font-montserrat h-min md:h-screen relative hide-scroll bg-indigo-white flex overflow-x-hidden overflow-y-hidden">
@@ -42,28 +39,6 @@ const Container = (props) => {
       <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KQBL75W"
       height="0" width="0" className="hidden"></iframe></noscript>
 
-      <div className="invisible w-0 md:visible md:w-full">
-        <div className="flex bg-indigo-white">
-          <DesktopNavbar
-            isAdmin={isAdmin}
-            isLoggedIn={selector.isSignedIn()}
-            color="indigo-navbargrad2"
-            secondcolor="indigo-navbargrad1"
-            activeName={activeName}
-          />
-          <div className="flex flex-col w-screen h-screen">
-            <DesktopHeaderBase />
-            {children}
-          </div>
-        </div>
-      </div>
-
-      <div className="visible md:invisible h-fit overflow-x-auto z-40">
-        <Navbar isAdmin={isAdmin} isLoggedIn={selector.isSignedIn()} />
-        <HeaderBase />
-
-        {children}
-      </div>
     </div>
   );
 };
