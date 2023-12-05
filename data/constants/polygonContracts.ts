c873fec16e1bfc28da9486a5e0a408f6f1aeaafb
Kendrell Derek Acibar <59480053+ciirno@users.noreply.github.com>
2023-12-05 20:35:35 +0800

    Update mainnet addresses

--- data/constants/polygonContracts.ts
+++ data/constants/polygonContracts.ts
@@ -4,8 +4,8 @@ export const ATHLETE_NFL_POLYGON = {
     logic: '0x6F2501fEC1E0470a58AF79B30c5Ca74d2a8DEc88',
   },
   mainnet: {
-    storage: '',
-    logic: '',
+    storage: '0xEd9381FDBA903285044AC452968991e707dBa432',
+    logic: '0x1EAB955408F672Fd4F97aAD0c0cb7b1f99a39e02',
   },
 };
 
@@ -15,8 +15,8 @@ export const PROMO_ATHLETE_NFL_POLYGON = {
     logic: '0x1Bb33843Ab6E4f71cE1d432adC9262e9A9d91044',
   },
   mainnet: {
-    storage: '',
-    logic: '',
+    storage: '0x10B5e5EB40fA28F20401260E71922c6e411A849b',
+    logic: '0x45b02F6C53d10C0e629278b61e97B43cb1a99781',
   },
 };
 
@@ -26,8 +26,8 @@ export const PACK_NFL_POLYGON = {
     logic: '0xC8F5326F774F4d84FE573333bEa5539a8184C777',
   },
   mainnet: {
-    storage: '',
-    logic: '',
+    storage: '0x58E2fB6582234982ed314B62e146FE69c3EDc363',
+    logic: '0x05Fbd90B8C1955745F7c4b16edC789e9052202F3',
   },
 };
 
@@ -37,8 +37,8 @@ export const PROMO_PACK_NFL_POLYGON = {
     logic: '0xa8C039983a03dAd358c9f52812F6D7Cad90211da',
   },
   mainnet: {
-    storage: '',
-    logic: '',
+    storage: '0x094D482993fbCe261d33739c9DED5f34450758Ef',
+    logic: '0x74E042128Bd94E0F7CdaF5996473990D65C1532e',
   },
 };
 
@@ -48,8 +48,8 @@ export const OPENPACK_NFL_POLYGON = {
     logic: '0xCb53993d12F68C16fA77FB9F1B01809c3969cEe7',
   },
   mainnet: {
-    storage: '',
-    logic: '',
+    storage: '0xdcbd023e9ACc6C2c21b4b5668eA936BC7225aa57',
+    logic: '0x85bB27cfeE9A0CE65a0F65D84eeda65368841B3d',
   },
 };
 
@@ -59,8 +59,8 @@ export const PROMO_OPENPACK_NFL_POLYGON = {
     logic: '0x2995E1DFCaf9943b50B3D7D4DF0116E207022eB4',
   },
   mainnet: {
-    storage: '',
-    logic: '',
+    storage: '0xbc20A96Af379C16B609106001CD3f030814918f4',
+    logic: '0xB551198bce7A81b40d65Bf2b0Df2B1bAc32721F9',
   },
 };
 
@@ -70,14 +70,14 @@ export const GAME_NFL_POLYGON = {
     logic: '0x38F84A16c42F63FD6E04D28d9a790529c64BA1c7',
   },
   mainnet: {
-    storage: '',
-    logic: '',
+    storage: '0x3702Ea05D7e6d17e31da61E9Fc6F6AfA637EAc99',
+    logic: '0x8DAddd1E5b502A208ceA59811B02A821dEbBA865',
   },
 };
 
 export const POL141USDC = {
   title: 'USDC',
-  mainnet: '',
+  mainnet: '0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174',
   testnet: '0x0FA8781a83E46826621b3BC094Ea2A0212e71B23',
   decimals: 1000000,
 }; //ERC-20 USDC for Polygon
